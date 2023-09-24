e1c1f39dc5e88b03a70bfb62545f285b7af3d3db
digimori <hanaorikuu@gmail.com>
2023-09-24 19:21:49 +0000

    Notes added on how to proceed.

--- Drumkit/drumScript.js
+++ Drumkit/drumScript.js
@@ -3,9 +3,11 @@ let noOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < noOfDrumButtons ; i++) {
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    alert("Clicked!");
+    console.log(this);
 })
-
 }
 
-
+// Remember, 'this' is like saying 'THIS thing in particular'
+// Should we switch statement this instead of if/else?
+/*var audio = new Audio('Drumkit/sounds/tom-1.mp3');
+    audio.play(); */
