1ac3b3bc6a64a1a1d999f1a9b580971e22dbd427
digimori <hanaorikuu@gmail.com>
2023-09-24 19:37:22 +0000

    Sound files added and assigned via switch statement

--- Drumkit/drumScript.js
+++ Drumkit/drumScript.js
@@ -3,7 +3,41 @@ let noOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < noOfDrumButtons ; i++) {
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    console.log(this);
+    let buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+        case "w":
+            var audio = new Audio('Drumkit/sounds/tom-1.mp3');
+            audio.play()
+            break;
+        case "a":
+            var audio = new Audio('Drumkit/sounds/tom-2.mp3');
+            audio.play()
+            break;
+        case "s":
+            var audio = new Audio('Drumkit/sounds/tom-3.mp3');
+            audio.play()
+            break;
+        case "d":
+            var audio = new Audio('Drumkit/sounds/tom-4.mp3');
+            audio.play()
+            break;
+        case "j":
+            var audio = new Audio('Drumkit/sounds/crash.mp3');
+            audio.play()
+            break;
+        case "k":
+            var audio = new Audio('Drumkit/sounds/kick-bass.mp3');
+            audio.play()
+            break;
+        case "l":
+            var audio = new Audio('Drumkit/sounds/snare.mp3');
+            audio.play()
+            break;
+    
+        default:
+            break;
+    }
 })
 }
 
